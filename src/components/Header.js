6f2a76a2154b4f74154f8b20f69f2a608ce905dc
slimkhemiri <khemiri.medslim1@gmail.com>
2022-05-17 16:09:01 +0100

    solve error

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Crypto Hunter
+              Crypto Pro
             </Typography>
             {/* <Button color="inherit">Login</Button> */}
             <Select
