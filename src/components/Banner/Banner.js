de704063098aba4f06ce72958524a3a10eaeebbb
slimkhemiri <khemiri.medslim1@gmail.com>
2023-02-26 14:37:30 +0000

    update changes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Crypto Pro
           </Typography>
           <Typography
             variant="subtitle2"
